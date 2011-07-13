6b29b6362305e37b4c4cd1e8033949685a3d0686
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2011-07-13 11:55:03 -0400

    Disable inbound buffering on TCP connections when measuring buffer size

--- determine-tcp-buffer-size.rkt
+++ determine-tcp-buffer-size.rkt
@@ -6,6 +6,7 @@
 
 (display "Listening\n")
 (define-values (i o) (tcp-accept listener))
+(file-stream-buffer-mode i 'none)
 (display "Accepted, sleeping 10\n")
 (sleep 10)
 (display "Reading chunk\n")
